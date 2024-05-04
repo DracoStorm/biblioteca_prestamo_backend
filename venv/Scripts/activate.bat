3df2b59c6889845978e75c9073805ee21467ec16
Jared Ubaldo Sanchez Lopez <jaredtoxic@gmail.com>
2024-05-04 17:25:49 -0600

    creacion de la carpeta defineModels y entorno virtual

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\moise\Desktop\4 SEMESTRE\biblioteca_prestamo_backend\venv
+set VIRTUAL_ENV=C:\Users\jared\Documents\proyecto\biblioteca_prestamo_backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
