e4003db8be5dd3b564de7738aa7dbdcd42c032b6
Moises MP <moisesmachorro52@gmail.com>
2024-05-04 20:24:23 -0600

    Agregacion de 'defineModels' y 'rest_framework' en INSTALLED_APPS

--- biblioteca_prestamo/settings.py
+++ biblioteca_prestamo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'defineModels',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
