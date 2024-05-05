a28342d3d6a653d4dc9c37cdb6357204ea699ae4
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2024-05-05 14:46:29 -0600

    examples

---
+++ defineModels/modelo.py
@@ -0,0 +1,41 @@
+from .models import Student
+
+# Acá va el MODELO, el que se encuentra en el diagrama, solo utiliza las clases de modelo para CONSULTAS
+
+
+class Usuario:
+    ...
+
+
+class Estudiante(Usuario):
+    ...
+
+
+class Administrador(Usuario):
+
+    def registrarEstudiante():
+        return
+
+    def actualizarEstudiante():
+        return
+
+    def eliminarEstudiante():
+        return
+
+    def registrarLibro():
+        return
+
+    def actualizarLibro():
+        return
+
+    def eliminarLibro():
+        return
+
+    def estadoPrestamoEstudiante():
+        return
+
+    def agregarPrestamoEstudiante():
+        return
+
+    def eliminarPrestamoEstudiante():
+        return
